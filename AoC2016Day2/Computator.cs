11395c25a73a87648ffb158c4754ecfd24870507
Bogdan <Faldarknashamir@gmail.com>
2017-06-08 12:07:36 +0300

    backup before changing Numpad logic

--- AoC2016Day2/Computator.cs
+++ AoC2016Day2/Computator.cs
@@ -11,16 +11,41 @@ namespace AoC2016Day2
 
         public List<int> ComputateResult(string[] inputData)
         {
+            List<Numpad> myList = new List<Numpad>();
+           
             foreach (var word in inputData)
             {
+                Numpad numpad = new Numpad();
+                if (myList.Any())
+                {
+                    numpad.Width = myList.Last().Width;
+                    numpad.Height = myList.Last().Height;
+                }
+
+                
                 foreach (var character in word)
                 {
                     if (character == 'U')
                     {
-                        
+                        ++numpad.Height;
+                    }
+                    else if (character == 'D')
+                    {
+                        --numpad.Height;
+                    }
+                    else if (character == 'L')
+                    {
+                        --numpad.Width;
+                    }
+                    else if(character == 'R')
+                    {
+                        ++numpad.Width;
                     }
                 }
+                myList.Add(numpad);
             }
+
+            return myList.Select(numpad => numpad.numpadnumbers[numpad.Height][numpad.Width]).ToList();
         }
 
 
